10ad3d7580db03f46e7e344915b668ca969b62be
briilala <48503246+briilala@users.noreply.github.com>
2019-12-01 23:34:56 -0500

    Add files via upload

---
+++ Sell.php
@@ -0,0 +1,80 @@
+<?php
+  include_once("header.html");
+  include('server.php');
+  include('errors.php');
+  // session_start();
+?>
+<?php
+if (isset($_POST['Post'])){
+  //TODO: add error message if something is not set. If picture not set, verify with customer?
+$ISBN = mysqli_real_escape_string($db, $_POST['ISBN']);
+$title = mysqli_real_escape_string($db, $_POST['Title']);
+$price = mysqli_real_escape_string($db, $_POST['Price']);
+$course = mysqli_real_escape_string($db, $_POST['CourseCode']);
+$description = mysqli_real_escape_string($db, $_POST['Description']);
+$contact = mysqli_real_escape_string($db, $_POST['Contact']);
+$user = $_SESSION['username'];
+$db = mysqli_connect("localhost", "root", "root", "bookfinder");
+// Get image name
+$image = $_FILES['image']['name'];
+if(file_exists($image))
+{
+  $target = "images/".basename($image);
+  if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+    $msg = "Image uploaded successfully";
+  }else{
+    $msg = "Failed to upload image";
+  }
+}
+else {
+  $image = 'NULL';
+}
+// image file directory
+
+$query = "INSERT INTO books(ISBN, title, price, courseCode, description, contact, picture, poster) Values(
+  '$ISBN', '$title', '$price', '$course', '$description', '$contact', '$image', '$user')";
+  //TODO: print success message if inserted correctly
+if(mysqli_query($db, $query))
+{
+  echo "recprd";
+}
+else {
+  echo "try again";
+}
+
+
+}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="homestyle2.css">
+</head>
+<body>
+
+  <div>
+      <!-- notification message -->
+      <?php  if (isset($_SESSION['username'])) : ?>
+        <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+      <?php endif ?>
+
+      <form method="post" action="" enctype="multipart/form-data" <?php echo $_SERVER['PHP_SELF']; ?>>
+        <?php include('errors.php'); ?>
+        <input type="text" class="search" name="ISBN" placeholder="ISBN"><br>
+        <input type="text" class="search" name="Title" placeholder="Title"><br>
+        <input type="text" class="search" name="Price" placeholder="Price"><br>
+        <input type="text" class="search" name="CourseCode" placeholder="Course Code"><br>
+        <input type="text" class="search" name="Description" placeholder="Description"><br>
+        <input type="text" class="search" name="Contact" placeholder="Contact Details"><br>
+        <input type="hidden" name="size" value="1000000">
+       	<div>
+       	  <input type="file" name="image">
+       	</div>
+        <input type="submit" class="search" name="Post" value="Post">
+      </form>
+
+  </div>
+</body>
+</html>
--- profile.php
+++ profile.php
@@ -24,20 +24,20 @@
     echo "No book posted";
   }
   else {
-  //   echo "no results";
-  // }
-   // $result = mysqli_query($db, "SELECT * FROM images");
     while ($row = $result->fetch_assoc())
-    // while($row = mysqli_fetch_array($result)
     {
-      // echo $row['ISBN'];
 		$bookid = $row['book_id'];
     $name = "btn" . $bookid;
-	//	echo "$bookid";
-	//	echo "<br>";
-	//	$delete_query = mysqli_query($db,"DELETE FROM books WHERE book_id=$bookid");
-      echo "<tr>";
-      echo "<td>";?> <img src="images/<?php echo $row['picture']; ?>" height="100" width="100"> <?php echo "</td>";
+
+        if($row['picture'] != 'NULL')
+        {
+          echo "<tr>";
+          echo "<td>"; ?> <img src="images/<?php echo $row['picture']; ?>" height="100" width="100"> <?php echo "</td>";
+      }
+      else {
+        echo "<tr>";
+        echo "<td>"; ?> <img src="images/nophoto.png" height="100" width="100"> <?php echo "</td>";
+      }
       ?>
       <div class="input-group">
 	  <form action="profile.php" method = "post">
@@ -56,29 +56,7 @@
 		?>
       <?php
       echo "<br>";
-      // " . "<img src='images/".$row['picture']."' >" . "</td></tr>";
-      //       	// echo "<p>".$row['image_text']."</p>";
           }
-      // echo print_r($row);
-      // echo "<br>";
-      // echo 'img src="data:image/jpeg;base64,'.base64_encode( $result['picture']).'"/>';
-      // while ($row2 = mysqli_fetch_array($results))
-      // {
-      //   echo "<img src='images/png".$row['picture']."' >";
-      //   echo "<p>".$row['image_text']."</p>";
-      // }
-      // echo "<br>";
-      // echo "<br>";
-      // echo "<tr><td>" . $row['ISBN'] . "</td></tr>"; //print row with image
-      // echo "<br>";
-        // while ($row2 = mysqli_fetch_array($results))
-        // {
-        //   echo "<img src='images/".$row2['image']."' >";
-        //   echo "<p>".$row2['image_text']."</p>";
-        // }
-    //
-    // }
-          // exit();
         }
           ?>
 <p> <a href="login.php?logout='1'" style="color: red;">logout</a> </p>
